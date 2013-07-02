05ec76a7a6ce41c8db1aec10d09386aeefe64c59
sebamontalban <seba.montalban@gmail.com>
2013-07-02 07:40:24 -0700

    Create config.php
    Configuración de conexión database

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+
+error_reporting(E_ALL ^ E_NOTICE);
+
+/*=========== Database Configuración ==========*/
+
+$db_host = 'localhost';
+$db_user = 'wimtech_test';
+$db_pass = 'testtest';
+$db_name = 'wimtech_wimtech';
+
+?>
